2e6c5008dee09620656b50f11e325a4e98854b08
12Fngas <mazasock@qq.com>
2019-08-23 23:23:15 +0800

    43课笔记

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
-const http = require('http');
-http.createServer(function () {
-    res.writeHead(200,{'Content-Type':'text/plain'});
+// var a = 12;
+// var b = a;
+// console.log(a);
 
-    res.end('hello node.js');
+// var arr1 = [12, 23];
+// var arr2 = arr1;
+// arr2.push(100);
+// console.log(arr1);
 
-}).listen(3000, 'localhost',function () {
-    console.log('Running~~~');
-});
+// function sum () 
+// {
+//     var total = null;
+//     for (var i = 0; i < arguments.length; i++)
+//     {
+//         var item = arguments[i];
+//         item = parseFloat(item);
+//         if (!isNaN(item))
+//         {
+//             total += item;
+//         }
+//     }
+//     return total;
+// }
 
-////
+// console.log(sum(12, 23 ,'34', 'AA'));
+
+
+
+
+
+// f = function () {
+//     return true;
+// }
+
+// g = function () {
+//     return false;
+// }
+
+// ~function () 
+// {
+//     if ( g() & [] == ![] ) 
+//     {
+//         f = function () { return false; };
+//         function g () { return true; }
+//     }
+
+// }();
+
+// console.log(f());
+// console.log(g());
+
+
+
+
+
+// if (1 === 2)
+// {
+//     console.log(fn);
+//     function fn ()
+//     {
+//         console.log('ok');
+//     }
+// }
+
+// console.log(fn);
+
+
