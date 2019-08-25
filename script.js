64eb52d13d365847345f14c2719fe11d192b7a55
12Fngas <mazasock@qq.com>
2019-08-26 00:51:17 +0800

    第50课

--- script.js
+++ script.js
@@ -115,4 +115,86 @@
 
 
 
+// var a = 12,
+//     b = 13,
+//     c = 14;
 
+// function fn (a)
+// {
+//     console.log(a, b, c); // => 12, undefined, 14
+//     var b = c = a = 20;
+//     console.log(a, b, c); // => 20, 20, 20
+// }
+
+// fn(a);
+// console.log(a, b, c); // => 12, 13, 20
+
+
+
+
+
+// var arr = [12, 23];
+// function fn (arr)
+// {
+//     console.log(arr); // => 12，23
+//     arr[0] = 100;
+//     arr = [100];
+//     arr[0] = 0;
+//     console.log(arr); // => 0
+// }
+// fn(arr);
+// console.log(arr); // => 100, 23
+
+
+
+// var a = 12;
+// function fn () 
+// {
+//         console.log(arguments.callee.caller);
+// }
+// function sum ()
+// {
+//     var a = 120;
+//     fn ();
+// }
+// function aa ()
+// {
+//     fn();
+// }
+// sum();
+
+
+
+// var n = 10;
+// function fn () 
+// {
+//     var n = 20;
+//     function f ()
+//     {
+//         n++;
+//         console.log(n);
+//     }
+//     f();
+//     return f;
+// }
+// var x = fn(); // 21
+// x(); // 22
+// x(); // 23
+// console.log(n); // 10
+
+
+
+var i = 1;
+function fn (i) 
+{
+    return function (n)
+    {
+        console.log(n + (++i));
+    }
+}
+
+var f = fn(2); // 2
+f(3); // 6
+fn(5)(6); // 12
+fn(7)(8); // 16
+f(4); // 9
