1ee7f4c95493b40b86ac0b8a192d2f8ee4091c63
12Fngas <mazasock@qq.com>
2019-08-28 20:38:34 +0800

    珠峰55课

--- script.js
+++ script.js
@@ -184,6 +184,7 @@
 
 
 
+
 // var i = 1;
 // function fn (i) 
 // {
@@ -197,3 +198,175 @@
 // fn(5)(6); // 12
 // fn(7)(8); // 16
 // f(4); // 8
+
+
+
+// // 闭包：函数执行形成一个私有作用域，保护里面的私有变量不受外界干扰
+// //闭包：柯理化函数
+// function fn ()
+// {
+//     return function ()
+//     {
+        
+//     }
+// }
+
+// //闭包：惰性函数
+// var utils = (function ()
+// {
+//     return {
+
+
+//     }
+// }
+// )();
+
+// //JQ方式：把需要暴露的方法抛到全局
+// (function ()
+// {
+//     function jQuery()
+//     {
+
+//     }
+//     window.jQuery = window.$ = jQuery;
+// }
+// )();
+// $();
+// jQuery();
+
+// //Zepto方式：基于return把需要供外部使用的方法暴露出去
+// var Zepto = (function ()
+// {
+//     //.....
+//     return {
+//         xxx : function () {}
+//     };
+// })();
+// Zepto.xxx();
+
+
+
+// var list = document.getElementsByTagName('li');
+// for (var i = 0; i < list.length; i++)
+// {
+//     /* 方法1： 自定义属性
+//     list[i].idx = i;
+//     list[i].onmouseover = function ()
+//     {
+//         console.log(this.idx);
+//     }
+//     */
+    
+//     /* 方法2： 闭包
+//     (function (i)
+//     {
+//         list[i].onmouseover = function ()
+//         {
+//             console.log(i);
+//         };
+//     }) (i);
+//     */
+
+//     /* 方法3：ES6 let
+//     for (let i = 0; i < list.length; i++)
+//     */
+// };
+
+
+
+// 单例设计模式（singleton pattern）
+
+// 1.表现形式
+// var obj = {
+//     name : 'xxx',
+//     age : 18
+// };
+// 在单例设计模式中，obj不仅是对象名，它被称为“命名空间[namespace]“，
+// 把描述事物的属性存放到明明空间中，对个命名空间独立分开，互不冲突
+
+// 2.作用：把描述同一件事物的属性和特征进行“分组、归类”（存储在同一个堆内存空间中），
+// 因此避免了全局变量之间的冲突和污染
+// var pattern1 = {name : 'xxx'};
+// var pattern2 = {name : 'xxx'};
+
+// 3.单例设计模式命名的由来
+// 每个命名空间都是JS中Object这个基类的实例，而实例之间是互相独立互不干扰的，
+// 所以我们称它为”单例（单独的实例）“
+
+// 高级单例模式：
+// var namespace = (function ()
+// {
+//     function fn1() {}
+//     function fn2() {}
+//     return {
+//         fn1 : fn1,
+//         fn2 : fn2
+//     };
+// })();
+
+
+
+
+// var n = 2;
+// var obj = {
+//     n : 3,
+//     fn : (function (n)
+//     {
+//         n *= 2;
+//         this.n += 2;
+//         var n = 5;
+//         return function (m)
+//         {
+//             this.n *= 2;
+//             console.log(m + (++n));
+//         }
+//     }) (n)
+// };
+// var fn = obj.fn;
+// fn(3);
+// obj.fn(3);
+// console.log(n, obj.n);
+
+// this
+// 1.给当前元素的某个事件绑定方法，当事件触发方法执行的时候，方法中的this是当前操作的元素对象
+// oBox.onclick = function ()
+// {
+//     // this === oBox
+// }
+
+// 2.普通函数执行，函数中的this取决于执行的主体，谁执行的，this就是谁（执行主体：方法执行，
+// 看方法名前面是否有‘点’，有的话，点前面是谁this就是谁，没有this是window）
+// function fn ()
+// {
+//     console.log(1);
+// }
+// var obj = {fn : fn};
+// obj.fn(); // ==> 1
+
+// 自执行函数执行，其this为window
+// ~function ()
+// {
+//     // this === window
+// }();
+
+
+
+
+模块化开发
+  1.团队协作开发的时候，会把产品按照功能板块进行划分，每一个功能
+  板块有专人负责开发
+  2.把各个板块之间公用的部门进行提取封装，后期在想实现这些功能，
+  直接调用引用即可（模块封装）
+
+var weatherRender = (function ()
+{
+    var fn = function () {};
+    // ...
+    return {
+        init : function () 
+        {
+            fn();
+        }
+    };
+})();
+weatherRender.init();
