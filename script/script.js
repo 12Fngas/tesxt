ffb97dfff3ea20397a2a1227f510f3fbabee6236
mazasock <mazasock@qq.com>
2019-11-05 14:54:32 +0800

    179^ css3新特性

--- script/script.js
+++ script/script.js
@@ -4767,6 +4767,125 @@ let change = {
  *   
  * 
  * 二：CSS3
+ *  学习一些样式属性和选择器就差不多了
+ * 
+ *  [选择器]
+ *  #ID  .CLASS  TAG  *  SELECTOR1,SELECTOR2(群组选择器)
+ * 
+ *      A B{} 后代
+ *      A>B{} 子代
+ *      A+B{} 下一个弟弟
+ *      A~B{} 兄弟
+ *      A.B{} 既具备A也具备.B的（统计二次筛选）
+ *      
+ *      A[NAME=''] 属性选择器 NAME!='' / NAME^=''  /  NAME$=''  / NAME*=''
+ *      ......
+ * 
+ *      A:HOVER
+ *      A:ACTIVE
+ *      A:VISITED
+ *      A:AFTER
+ *      A:BEFORE
+ * 
+ *      A:NTH-CHILD
+ *      A:NTH-LAST-CHILD
+ *      A:NTH-OF-TYPE
+ *      A:NTH-LAST-OF-TYPE
+ *      A:NOT
+ *      A:FIRST-CHILD
+ *      A:LAST-CHILD
+ *      ......
+ *      
+ *  [样式属性]
+ *   1.基本常用的
+ *    border-radius
+ *    box-shadow
+ *    text-shadow
+ * 
+ *   2.背景的
+ *    background-color / -image  / -position  / -repeat  /  -attachment  /  ...
+ *    background-size:
+ *      100px 100px  宽高具体指
+ *      100% 100%  宽高百分比（相对所在容器）
+ *      cover  以合适比例把图片缩放（不会变形），用来覆盖整个容器
+ *      contain  背景图覆盖整个容器（但是会出现，如果一边碰到容器的边缘，则停止覆盖，导致部分区域无背景图）   
+ *    background-clip: 背景图片裁剪
+ *      border-box
+ *      padding-box
+ *      content-box
+ *    background-origin: 设置背景图起始点
+ *      ...
+ *    filter: 滤镜
+ *      
+ *      
+ *    [CSS3动画和变形] （2D、3D）
+ * 
+ *     变形不是动画
+ *     transform：
+ *       translate(x|y|z) 位移
+ *       scale 缩放
+ *       rotate 旋转
+ *       skew 倾斜
+ *       matrix 矩阵（按照自己设定的矩阵公式实现变形）
+ * 
+ *     transform-style: preserve-3d 实现3D变形
+ *     transform-origin: 变形的起点
+ * 
+ * 
+ *     过渡动画
+ *     transition：
+ *       transition-property: all / width ... 哪些属性样式发生改变执行过渡动画效果，默认all，所有样式
+ *     属性改变都会执行这个过渡效果。
+ *       transition-duration: 过渡动画时间，我们一般都用秒，如 5.s
+ *       transition-timing-function: 动画运动的方式 linear(默认)、ease-in、ease-out、
+ *     ease-in-out、cubic-bezier（执行总监设定的贝塞尔曲线）
+ *       transition-delay: 设置延迟的时间，默认是0s不延迟，立即执行动画
+ *       ......
+ * 
+ *     帧动画
+ *     animation:
+ *       animation-name  运动轨迹的名称
+ *       animation-duration  运动时长
+ *       animation-timing-function  运动的方式（默认ease）
+ *       animation-delay  延迟时间
+ *       animation-iteration-count  运动次数（默认1  infinite无限次运动）
+ *       animation-fill-mode  运动完成后的状态（帧动画完成后，元素会默认回到运动的其实位置，
+ *     如果想让其停留在最后一帧的位置，设置这个属性值为forwards； backwards是当前帧动画如果
+ *     有延迟时间，在延迟等待时间内，元素处于帧动画的第一帧位置； both是让帧动画同时具备forwards
+ *     和backwards）
+ *       ......
+ * 
+ *     设置帧动画的运动轨迹
+ *     @keyframes [运动轨迹名称] {
+ *       from{
+ *         // 开始的样式
+ *       }
+ *       to{
+ *         // 结束的样式 
+ *       }
+ *     }
+ * 
+ * 
+ *    [css3中新盒子模型]
+ *       box-sizing: border-box / padding-box / content-box(默认值)
+ *       改变的就是我们在css中设置的width 或 height到底代表啥，border-box让其代表整个盒子的宽高，
+ *     当我们去修改padding 或者 border，盒子大小不变，只会让内容缩放。
+ * 
+ *       columns：多列布局
+ * 
+ *       flex：弹性盒子模型
+ *       
+ *    [其他css3属性]
+ *      perspective：视距  实现3D动画必用的属性
+ *      @media：媒体查询  实现响应式布局的一种方案
+ *      @font-face：导入字体图标
+ *      ......
+ *     
+ * 
+ *     T1.1 ~ 1.3  p1 ~ p3  初级
+ *     T2.1 ~ 2.3  p4 ~ p6  中高级
+ *     T3.1 ~ 3.3  p7 ~ p9  架构师
+ *      
  * 
  * 三：响应式布局开发
  * 
